89737b807873e1d99f6dbdecd2b23a19805f502b
Shameel Kiyang <217050743@cput.ac.za>
2021-03-24 14:38:36 +0200

    First commit

--- src/za/ac/cput/calculator/Division.java
+++ src/za/ac/cput/calculator/Division.java
@@ -1,5 +1,8 @@
 package za.ac.cput.calculator;
 
+/**
+ * @author Shameel
+ */
 public class Division {
     public static int integerDivision(int numerator, int denominator){
         if (denominator < 1) return 0;
